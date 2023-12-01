32a1f22563b0171362dfff01e53b4c8d16b74d3b
Thirumalakonda-Ganesh <ganitthirumala50@gmail.com>
2023-12-01 11:48:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { useForm } from "react-hook-form";
+import './App.css'
 function App() {
+  const {
+    register,
+    formState: { errors },
+    trigger,
+    handleSubmit,
+    clearErrors
+  } = useForm({
+    mode: "onChange"
+  });
+
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>First name: </label>
+      <input {...register("firstName", { required: true })} />
+      {errors.firstName && <p>This field is Required</p>}
+
+      <label>Last name: </label>
+      <input {...register("lastName", { required: true })} />
+      {errors.lastName && <p>This field is Required</p>}
+
+      <label>Username: </label>
+      <input {...register("username", { required: true })} />
+      {errors.username && <p>This field is Required</p>}
+      <button
+        type="button"
+        onClick={() => {
+          clearErrors();
+        }}
+      >
+        Clear Errors
+      </button>
+      <input type="submit" />
+      <button
+        type="button"
+        onClick={() => {
+          trigger();
+        }}
+      >
+        Validate All
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          trigger("firstName");
+        }}
+      >
+        Validate First Name
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          trigger(["firstName", "lastName"]);
+        }}
+      >
+        Validate First And Last Name
+      </button>
+      <button
+        type="button"
+        onClick={async () => {
+          console.log("firstName", await trigger("firstName"));
+        }}
+      >
+        Trigger Async First Name Validation
+      </button>
+    </form>
   );
 }
-
-export default App;
+export default App
